65ea70ea8f3ccd544ce4b2bb197284544150e8b5
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-12-03 14:37:01 -0800

    feat(templating): replace handlebars with ejs

--- src/lib/templating.js
+++ src/lib/templating.js
@@ -2,66 +2,62 @@
 
 const path = require('path');
 
-const debug = require('debug')('proj:lib:templating');
-const handlebars = require('handlebars');
+const _ = require('lodash');
 const {
   copyFile, readFile, writeFile
 } = require('mz/fs');
 const mkdirp = require('mkdirp');
 
+const {d: debug, f} = require('./debug')(__filename);
+
 /**
  * Write handlebars templates into current dir.
- *
  * @param {string} filename - template filename
- * @param {object} context - argv
- * @returns {Promise} -
+ * @param {Object} context - argv
  */
 exports.template = async function template(filename, context) {
-  debug(`Templating ${filename} into project`);
+  debug(f`Templating ${filename} into project`);
 
   let tpl;
   try {
-    debug(`Checking for ${filename} with hbs extension`);
-    tpl = await readFile(path.resolve(__dirname, '..', 'templates', `${filename}.hbs`), 'utf8');
-    debug(`Found ${filename} with hbs extension`);
+    debug(f`Checking for ${filename} with ejs extension`);
+    tpl = await readFile(path.resolve(__dirname, '..', 'templates', `${filename}.ejs`), 'utf8');
+    debug(f`Found ${filename} with ejs extension`);
   }
   catch (err) {
-    debug(`Checking for ${filename} without hbs extension`);
+    debug(f`Checking for ${filename} without ejs extension`);
     tpl = await readFile(path.resolve(__dirname, '..', 'templates', filename), 'utf8');
-    debug(`Found for ${filename} with hbs extension`);
+    debug(f`Found for ${filename} with ejs extension`);
   }
-  const out = handlebars.compile(tpl)(context);
+  const out = _.template(tpl)(context);
   const dest = path.resolve(process.cwd(), filename);
   const dir = path.dirname(dest);
 
-  debug(`Ensuring directory ${dir}`);
+  debug(f`Ensuring directory ${dir}`);
   mkdirp.sync(dir);
   debug('Done');
 
-  debug(`Writing template to ${dest}`);
+  debug(f`Writing template to ${dest}`);
   await writeFile(dest, out);
   debug('Done');
 };
 
 /**
  * Copy a file to the project directory
- *
  * @param {string} filename - local file
- * @param {object} context - argv
- * @returns {Promise} -
  */
 exports.copy = async function copy(filename) {
-  debug(`Copying ${filename} into project`);
+  debug(f`Copying ${filename} into project`);
 
   const src = path.resolve(__dirname, '..', 'templates', filename);
   const dest = path.resolve(process.cwd(), filename);
   const dir = path.dirname(dest);
 
-  debug(`Ensuring directory ${dir}`);
+  debug(f`Ensuring directory ${dir}`);
   mkdirp.sync(dir);
   debug('Done');
 
-  debug(`Copying ${src} to ${dest}`);
+  debug(f`Copying ${src} to ${dest}`);
   await copyFile(src, dest);
   debug('Done');
 };
