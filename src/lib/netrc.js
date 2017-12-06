5e4b3f52cea52e56aa5afc44f4aaf4a5f2977c54
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2017-12-05 21:27:58 -0800

    refactor(netrc): use debug helper

--- src/lib/netrc.js
+++ src/lib/netrc.js
@@ -1,20 +1,21 @@
 'use strict';
 
-const debug = require('debug')('proj:lib:netrc');
 const request = require('request-promise-native');
 const netrc = require('netrc');
+
+const {d: debug, f} = require('./debug')(__filename);
 
 exports.check = async function check(machine, testurl) {
   const config = netrc();
   const auth = config[machine];
 
   if (!auth) {
-    debug(`auth missing for ${machine}`);
+    debug(f`auth missing for ${machine}`);
     throw new Error(`please configure netrc for ${machine}`);
   }
 
   try {
-    debug(`verifying creds for ${machine}`);
+    debug(f`verifying creds for ${machine}`);
     const opts = {
       auth: {
         pass: auth.password,
@@ -25,11 +26,11 @@ exports.check = async function check(machine, testurl) {
       url: testurl
     };
     await request(opts);
-    debug(`verified creds for ${machine}`);
+    debug(f`verified creds for ${machine}`);
   }
   catch (err) {
     debug(err.message);
-    debug(`request to ${machine} failed; please re-invoke with the corresponding command line switches`);
+    debug(f`request to ${machine} failed; please re-invoke with the corresponding command line switches`);
   }
 };
 
