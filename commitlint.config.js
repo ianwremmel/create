3a4541974a5ca446501b537e7d70c5ffe91e5509
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-09-03 09:24:50 -0700

    ci(commitlint): change to config-conventional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,7 @@
 'use strict';
 
-// greenkeeper still uses the old commit message syntax, so we can't lint those
-// commits just yet
-if (process.env.CIRCLE_BRANCH && process.env.CIRCLE_BRANCH.includes('greenkeeper')) {
-  module.exports = {};
-}
-else {
-  module.exports = {
-    extends: [
-      '@commitlint/config-angular'
-    ]
-  };
-}
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ]
+};
