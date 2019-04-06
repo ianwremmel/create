1cbb288032f1f9f66ad0c506122e7f0ff7d0fa77
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2019-04-06 00:49:50 -0700

    fix(dependabot): use update api string arguments (undocumented)

--- src/lib/dependabot.js
+++ src/lib/dependabot.js
@@ -18,7 +18,8 @@ async function follow({githubRepoObject, githubUserObject}, github) {
   const {
     data: {token, id: tokenId},
   } = await github.oauthAuthorizations.createAuthorization({
-    note: 'Temporary token for @ianwremmel/create',
+    note:
+      'Temporary token for @ianwremmel/create to call CircleCI. If you are not current running the create script, you can delete this token.',
     scopes: ['repo'],
   });
   debug('Created temporary github token');
@@ -28,8 +29,8 @@ async function follow({githubRepoObject, githubUserObject}, github) {
       body: {
         'account-id': githubUserObject.id,
         'account-type': 'user',
-        'automerge-rule-development-deps': 'minor',
-        'automerge-rule-runtime-deps': 'minor',
+        'automerge-rule-development-deps': 'in_range',
+        'automerge-rule-runtime-deps': 'in_range',
         directory: '/',
         'package-manager': 'npm_and_yarn',
         'repo-id': githubRepoObject.id,
