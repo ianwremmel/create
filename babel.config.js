d48aa949299ef56964ee51ddd420e5da3aefd0f4
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2019-04-06 00:59:11 -0700

    build: add babel with typescript support

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(api) {
+  api.cache(false);
+
+  return {
+    comments: false,
+    presets: [
+      '@babel/preset-typescript',
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: true,
+          },
+        },
+      ],
+    ],
+  };
+};
